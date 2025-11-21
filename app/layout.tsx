80bf8e8f0feb328fa50333716f0befe261144199
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-21 19:13:34 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import "./globals.css";
-import React from "react";
+import "../globals.css"
+import React from "react"
 
 export const metadata = {
   title: "Everflow Recruitment Dashboard",
   description: "AI Recruitment SaaS",
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
@@ -17,5 +17,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  );
+  )
 }
