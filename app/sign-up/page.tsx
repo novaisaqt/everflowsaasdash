5340172df93266e4e89680f075fb38410bc73b47
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-22 01:47:26 +0000

    Update page.tsx

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-[#020617]">
-      <SignUp afterSignUpUrl="/dashboard" />
-    </div>
-  );
+  return <SignUp afterSignUpUrl="/dashboard" />;
 }
