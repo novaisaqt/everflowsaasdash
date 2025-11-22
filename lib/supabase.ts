a9caedddf063ff05861042a2c453094ce5d0e4b1
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-22 02:32:10 +0000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,12 @@
+// src/lib/supabase.ts
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+export const supabaseBrowser = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: { persistSession: true }
+});
+
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
