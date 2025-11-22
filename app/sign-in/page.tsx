b3a04bc68f9ead12bb8fe2088f3a01bf454f6cf1
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-22 02:35:17 +0000

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,9 +1,10 @@
-import { SignIn } from "@clerk/nextjs"
+// src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+import { SignIn } from '@clerk/nextjs';
 
-export default function Page() {
+export default function SignInPage() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-[#0f172a]">
+    <div className="min-h-screen flex items-center justify-center bg-neutral-950">
       <SignIn />
     </div>
-  )
+  );
 }
