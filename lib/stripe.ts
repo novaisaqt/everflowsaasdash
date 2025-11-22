e257b968dbe6fdc737c7a099d2ffd5020f5c8049
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-22 02:33:06 +0000

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,1 +1,12 @@
+// src/lib/stripe.ts
+import Stripe from 'stripe';
 
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2024-06-20',
+});
+
+export const plans = {
+  starter: process.env.NEXT_PUBLIC_STRIPE_PRICE_STARTER!,
+  growth: process.env.NEXT_PUBLIC_STRIPE_PRICE_GROWTH!,
+  scale: process.env.NEXT_PUBLIC_STRIPE_PRICE_SCALE!,
+};
