33b47fbf6f6a13382a897987eb2a3d28b15f030e
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-22 02:07:40 +0000

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs"
 
 export default function Page() {
-  return <SignIn afterSignInUrl="/dashboard" />;
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-[#0f172a]">
+      <SignIn />
+    </div>
+  )
 }
