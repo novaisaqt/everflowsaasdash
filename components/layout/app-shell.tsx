20151ba774ea29cdf41271293860da07f3692d9f
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-23 01:31:34 +0000

    Update app-shell.tsx

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -1,18 +1,7 @@
-'use client'
-
-import { ReactNode } from 'react'
-import { SignedIn } from '@clerk/nextjs'
-import Sidebar from '@/components/layout/sidebar'
-
-export default function AppShell({ children }: { children: ReactNode }) {
+export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <SignedIn>
-      <div className="min-h-screen flex bg-neutral-950 text-neutral-50">
-        <Sidebar />
-        <div className="flex-1 flex flex-col">
-          {children}
-        </div>
-      </div>
-    </SignedIn>
+    <div className="min-h-screen bg-gray-100 text-black">
+      <main className="ml-64">{children}</main>
+    </div>
   )
 }
