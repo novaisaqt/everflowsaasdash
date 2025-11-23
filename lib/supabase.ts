a7c9bf46312b4f389442a86d5a4bc7e209af8d00
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-23 02:54:54 +0000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export default supabase
+export const supabase = createClient(supabaseUrl, supabaseKey)
