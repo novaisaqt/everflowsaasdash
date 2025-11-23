2fb868fc8ab8a402348da1ffc71205b8006f6345
novaisaqt <83275700+novaisaqt@users.noreply.github.com>
2025-11-23 03:13:32 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/((?!_next|favicon.ico|.*\\.(?:png|jpg|jpeg|svg|css|js)).*)"],
+  matcher: []
 }
